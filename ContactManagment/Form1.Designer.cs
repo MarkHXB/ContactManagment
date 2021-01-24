a2544922143868b33f760d9fe46add75ed9b67c7
Márk Bakonyi <bakonyimark8@gmail.com>
2021-01-24 13:38:59 +0100

    LoginForm style

--- ContactManagment/Form1.Designer.cs
+++ ContactManagment/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ContactManagment
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,210 @@ namespace ContactManagment
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.toolMenuPanel = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.toolMenuHelpPanel = new System.Windows.Forms.Panel();
+            this.linkLblLogin = new System.Windows.Forms.LinkLabel();
+            this.lblAccountName = new System.Windows.Forms.Label();
+            this.txtboxAccountName = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtboxPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.toolMenuChangePanel = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pctboxHungarian = new System.Windows.Forms.PictureBox();
+            this.pctboxEnglish = new System.Windows.Forms.PictureBox();
+            this.toolMenuPanel.SuspendLayout();
+            this.toolMenuHelpPanel.SuspendLayout();
+            this.toolMenuChangePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctboxHungarian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctboxEnglish)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolMenuPanel
+            // 
+            this.toolMenuPanel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.toolMenuPanel.Controls.Add(this.btnMinimize);
+            this.toolMenuPanel.Controls.Add(this.btnExit);
+            this.toolMenuPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toolMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.toolMenuPanel.Name = "toolMenuPanel";
+            this.toolMenuPanel.Size = new System.Drawing.Size(547, 79);
+            this.toolMenuPanel.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(485, 22);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(50, 42);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.Location = new System.Drawing.Point(420, 22);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(50, 42);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            // 
+            // toolMenuHelpPanel
+            // 
+            this.toolMenuHelpPanel.Controls.Add(this.linkLblLogin);
+            this.toolMenuHelpPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toolMenuHelpPanel.Location = new System.Drawing.Point(0, 79);
+            this.toolMenuHelpPanel.Name = "toolMenuHelpPanel";
+            this.toolMenuHelpPanel.Size = new System.Drawing.Size(547, 39);
+            this.toolMenuHelpPanel.TabIndex = 1;
+            // 
+            // linkLblLogin
+            // 
+            this.linkLblLogin.AutoSize = true;
+            this.linkLblLogin.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLblLogin.Location = new System.Drawing.Point(12, 3);
+            this.linkLblLogin.Name = "linkLblLogin";
+            this.linkLblLogin.Size = new System.Drawing.Size(137, 31);
+            this.linkLblLogin.TabIndex = 0;
+            this.linkLblLogin.TabStop = true;
+            this.linkLblLogin.Text = "Logging in";
+            // 
+            // lblAccountName
+            // 
+            this.lblAccountName.AutoSize = true;
+            this.lblAccountName.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAccountName.Location = new System.Drawing.Point(187, 231);
+            this.lblAccountName.Name = "lblAccountName";
+            this.lblAccountName.Size = new System.Drawing.Size(185, 31);
+            this.lblAccountName.TabIndex = 2;
+            this.lblAccountName.Text = "Account Name";
+            // 
+            // txtboxAccountName
+            // 
+            this.txtboxAccountName.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxAccountName.Location = new System.Drawing.Point(172, 265);
+            this.txtboxAccountName.Name = "txtboxAccountName";
+            this.txtboxAccountName.Size = new System.Drawing.Size(219, 33);
+            this.txtboxAccountName.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLogin.Location = new System.Drawing.Point(209, 404);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(148, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Enter";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtboxPassword
+            // 
+            this.txtboxPassword.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxPassword.Location = new System.Drawing.Point(152, 351);
+            this.txtboxPassword.Name = "txtboxPassword";
+            this.txtboxPassword.Size = new System.Drawing.Size(256, 33);
+            this.txtboxPassword.TabIndex = 6;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPassword.Location = new System.Drawing.Point(224, 317);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(122, 31);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            // 
+            // toolMenuChangePanel
+            // 
+            this.toolMenuChangePanel.Controls.Add(this.pctboxEnglish);
+            this.toolMenuChangePanel.Controls.Add(this.pctboxHungarian);
+            this.toolMenuChangePanel.Controls.Add(this.linkLabel1);
+            this.toolMenuChangePanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolMenuChangePanel.Location = new System.Drawing.Point(0, 530);
+            this.toolMenuChangePanel.Name = "toolMenuChangePanel";
+            this.toolMenuChangePanel.Size = new System.Drawing.Size(547, 100);
+            this.toolMenuChangePanel.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 41);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(202, 23);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgot your password?";
+            // 
+            // pctboxHungarian
+            // 
+            this.pctboxHungarian.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pctboxHungarian.Location = new System.Drawing.Point(465, 10);
+            this.pctboxHungarian.Name = "pctboxHungarian";
+            this.pctboxHungarian.Size = new System.Drawing.Size(61, 37);
+            this.pctboxHungarian.TabIndex = 2;
+            this.pctboxHungarian.TabStop = false;
+            // 
+            // pctboxEnglish
+            // 
+            this.pctboxEnglish.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pctboxEnglish.Location = new System.Drawing.Point(465, 60);
+            this.pctboxEnglish.Name = "pctboxEnglish";
+            this.pctboxEnglish.Size = new System.Drawing.Size(61, 37);
+            this.pctboxEnglish.TabIndex = 3;
+            this.pctboxEnglish.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(547, 630);
+            this.Controls.Add(this.toolMenuChangePanel);
+            this.Controls.Add(this.txtboxPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtboxAccountName);
+            this.Controls.Add(this.lblAccountName);
+            this.Controls.Add(this.toolMenuHelpPanel);
+            this.Controls.Add(this.toolMenuPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
             this.Text = "Form1";
+            this.toolMenuPanel.ResumeLayout(false);
+            this.toolMenuHelpPanel.ResumeLayout(false);
+            this.toolMenuHelpPanel.PerformLayout();
+            this.toolMenuChangePanel.ResumeLayout(false);
+            this.toolMenuChangePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctboxHungarian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctboxEnglish)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel toolMenuPanel;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel toolMenuHelpPanel;
+        private System.Windows.Forms.LinkLabel linkLblLogin;
+        private System.Windows.Forms.Label lblAccountName;
+        private System.Windows.Forms.TextBox txtboxAccountName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtboxPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Panel toolMenuChangePanel;
+        private System.Windows.Forms.PictureBox pctboxEnglish;
+        private System.Windows.Forms.PictureBox pctboxHungarian;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
